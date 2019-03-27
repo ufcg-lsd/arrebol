6141746b8ef3d83b7aa83e53354737a0758a3ce1
Thiago Manel <thiago.manel@gmail.com>
2019-03-26 23:29:33 -0300

    add test for version endpoint

--- accept/accept_tests.py
+++ accept/accept_tests.py
@@ -1,12 +1,25 @@
 import unittest
 from arrebol_facade import Arrebol
 
-arrebolUrl=""
+arrebolUrl="http://localhost:8080"
 
 class AcceptTests(unittest.TestCase):
+
     def test_version(self):
         self.facade = Arrebol(arrebolUrl)
-        self.assertTrue(False)
+
+        #it asserts it does not error-terminate in error and version is a not
+        #empty string
+        response = self.facade.version()
+
+        self.assertStatusCode(response, 201)
+
+        actualMessage = response.json()['message']
+        self.assertIsInstance(actualMessage, str)
+        self.assertNotEqual(actualMessage, "")
+
+    def assertStatusCode(self, response, expectedStatusCode):
+        return response.status_code == expectedStatusCode
 
 if __name__ == '__main__':
     unittest.main()
