b6082c09e877153f040ae7a3c539426a3d8f7d96
Thiago Manel <thiago.manel@gmail.com>
2019-03-30 19:25:10 -0300

    add test support for create endpoint

--- accept/accept_tests.py
+++ accept/accept_tests.py
@@ -5,6 +5,9 @@ arrebolUrl="http://localhost:8080"
 
 class AcceptTests(unittest.TestCase):
 
+    def assertStatusCode(self, response, expectedStatusCode):
+        self.assertEquals(response.status_code, expectedStatusCode)
+
     def test_version(self):
         self.facade = Arrebol(arrebolUrl)
 
@@ -12,7 +15,7 @@ class AcceptTests(unittest.TestCase):
         #empty string
         response = self.facade.version()
 
-        self.assertStatusCode(response, 201)
+        self.assertStatusCode(response, 200)
 
         #TODO: need to understand better the response method
         #the code I borrowed uses response.json()['message'] instead
@@ -20,8 +23,12 @@ class AcceptTests(unittest.TestCase):
         #TODO: replaces by a pattern match a.b.c?
         self.assertNotEqual(actualMessage, "")
 
-    def assertStatusCode(self, response, expectedStatusCode):
-        return response.status_code == expectedStatusCode
+    def test_create(self):
+        self.facade = Arrebol(arrebolUrl)
+
+        response = self.facade.create("jobs/simple_job.json")
+        self.assertStatusCode(response, 201)
+        #TODO: assert job id and other stuff
 
 if __name__ == '__main__':
     unittest.main()
