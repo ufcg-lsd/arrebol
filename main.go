0bf5fb48e009e4c1f692ebba0da13797939613f8
Thiago Manel <thiago.manel@gmail.com>
2019-04-04 21:59:52 -0300

    add create endpoint. it closes #6

--- main.go
+++ main.go
@@ -11,8 +11,17 @@ func GetVersion(w http.ResponseWriter, r *http.Request) {
     json.NewEncoder(w).Encode("1.0")
 }
 
+func CreateJob(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusCreated)
+    json.NewEncoder(w).Encode("1.0")
+}
+
 func main() {
+
 	router := mux.NewRouter()
+
     router.HandleFunc("/version", GetVersion).Methods("GET")
+    router.HandleFunc("/create", CreateJob).Methods("POST")
+
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
