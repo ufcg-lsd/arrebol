bbfaa34535dce5791f89e8d65e428743fcd5964b
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-04-23 15:50:02 -0300

    Added new features to DockerWorkers

--- src/main/java/org/fogbowcloud/arrebol/resource/DockerWorker.java
+++ src/main/java/org/fogbowcloud/arrebol/resource/DockerWorker.java
@@ -1,61 +1,126 @@
 package org.fogbowcloud.arrebol.resource;
 
+import org.apache.log4j.Logger;
 import org.fogbowcloud.arrebol.core.models.command.Command;
 import org.fogbowcloud.arrebol.core.models.task.Task;
+import org.fogbowcloud.arrebol.resource.exceptions.DockerStartException;
 
 public class DockerWorker implements Worker {
 
-    private String containerId;
+    private String imageId;
+    private String containerName;
 
-    public DockerWorker(String containerId) {
-        this.containerId = containerId;
+    private final String BASH = "/bin/bash";
+    private final String DOCKER_EXEC = "sudo docker exec";
+    private final String DOCKER_RUN = "sudo docker run --rm -idt --name";
+    private final String DOCKER_STOP = "sudo docker stop";
+
+    private final Logger LOGGER = Logger.getLogger(DockerWorker.class);
+
+    public DockerWorker(String imageId, String containerName) {
+        this.imageId = imageId;
+        this.containerName = containerName;
     }
 
     @Override
     public TaskExecutionResult execute(Task task) {
+        TaskExecutionResult taskExecutionResult;
+        Integer startStatus = this.start();
+
+        if(startStatus != 0){
+            LOGGER.error("Exit code from container start: " + startStatus);
+            throw new DockerStartException("Could not start container " + this.containerName);
+        }
+
+        LOGGER.info("Successful started container " + this.containerName);
+
         int commandsSize = task.getCommands().size();
         Command[] commands = task.getCommands().toArray(new Command[commandsSize]);
         int[] commandsResults = new int[commandsSize];
-        for(int i = 0; i < commandsSize; i++) {
+
+        LOGGER.info("Starting to execute commands of task " + task.getId());
+        for (int i = 0; i < commandsSize; i++) {
             Command c = commands[i];
-            Integer exitCode = executeCommand(this.containerId, c);
+            Integer exitCode = executeCommand(c);
             commandsResults[i] = exitCode;
         }
 
+        Integer stopStatus = this.stop();
+        if(stopStatus != 0){
+            LOGGER.error("Exit code from container stop: " + stopStatus);
+        }
+
         TaskExecutionResult.RESULT result = TaskExecutionResult.RESULT.SUCCESS;
-        for(int i : commandsResults) {
+        for (int i : commandsResults) {
             if (i != 0) {
                 result = TaskExecutionResult.RESULT.FAILURE;
                 break;
             }
         }
-        
-        TaskExecutionResult taskExecutionResult = new TaskExecutionResult(result, commandsResults, commands);
+
+        LOGGER.info("Result of task" + task.getId() + ": " + result.toString());
+
+        taskExecutionResult = new TaskExecutionResult(result, commandsResults, commands);
         return taskExecutionResult;
     }
 
-    private Integer executeCommand(String containerId, Command command){
-        Integer exitCode = new Integer(111);
+    private Integer start() {
         try {
-            String commandString = command.getCommand();
             String[] cmd = {
-                    "/bin/bash",
+                    BASH,
                     "-c",
-                    "sudo docker exec " + containerId + " " + commandString
+                    DOCKER_RUN + " " + this.containerName + " " + this.imageId
+            };
+            Process p = Runtime.getRuntime().exec(cmd);
+
+            Integer exitCode = p.waitFor();
+            return exitCode;
+        } catch (Exception e){
+            throw new RuntimeException("Error while trying execute commands to start container " + this.containerName);
+        }
+    }
+
+    private Integer stop() {
+        try {
+            String[] cmd = {
+                    BASH,
+                    "-c",
+                    DOCKER_STOP + " " + this.containerName
+            };
+            Process p = Runtime.getRuntime().exec(cmd);
+
+            Integer exitCode = p.waitFor();
+            return exitCode;
+
+        } catch (Exception e){
+            throw new RuntimeException("Error while trying execute commands to stop container " + this.containerName);
+        }
+    }
+
+    private Integer executeCommand(Command command) {
+        try {
+            String commandStr = command.getCommand();
+            String[] cmd = {
+                    BASH,
+                    "-c",
+                    DOCKER_EXEC + " " + this.containerName + " " + commandStr
             };
 
             Process p = Runtime.getRuntime().exec(cmd);
 
-            exitCode = p.waitFor();
+            Integer exitCode = p.waitFor();
+            return exitCode;
 
         } catch(Exception e){
-            e.printStackTrace();
+            throw new RuntimeException("Error while truing execute commands to container " + this.containerName);
         }
-        return exitCode;
-
     }
 
-    public String getContainerId(){
-        return this.containerId;
+    public String getImageId(){
+        return this.imageId;
+    }
+
+    public String getContainerName() {
+        return this.containerName;
     }
 }
