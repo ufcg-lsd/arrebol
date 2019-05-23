7101210f03a70823f9422c6492dc122e97e5609a
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-05-23 16:37:16 -0300

    Updated fogbow_add_workers.py

---
+++ workers/fogbow_data.py
@@ -0,0 +1,15 @@
+ras_url='https://services-atm-test-site2.lsd.ufcg.edu.br/ras'
+ras_public_key_endpoint=ras_url + '/publicKey'
+ras_public_ip_endpoint=ras_url + '/publicIps'
+ras_compute_endpoint=ras_url + '/computes'
+
+ras_member_id=
+ras_cloud_name=
+ras_images=ras_url + '/images/' + ras_member_id + '/' + ras_cloud_name 
+
+as_url='https://services-atm-test-site2.lsd.ufcg.edu.br/as'
+as_token_endpoint= as_url + '/tokens'
+as_token_username=
+as_token_password=
+as_token_domain=
+as_token_project_name=
