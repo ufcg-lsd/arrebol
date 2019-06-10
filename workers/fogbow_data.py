d019b31ab3917488c031b30577ea43706a2dc636
Emanuel Joívo <emanueljoivo@gmail.com>
2019-06-10 00:14:27 -0300

    Exposed data removed

--- workers/fogbow_data.py
+++ workers/fogbow_data.py
@@ -1,4 +1,4 @@
-ras_url='https://services-atm-test-site2.lsd.ufcg.edu.br/ras'
+ras_url=''
 ras_public_key_endpoint=ras_url + '/publicKey'
 ras_public_ip_endpoint=ras_url + '/publicIps'
 ras_compute_endpoint=ras_url + '/computes'
@@ -7,7 +7,7 @@ ras_member_id=
 ras_cloud_name=
 ras_images=ras_url + '/images/' + ras_member_id + '/' + ras_cloud_name 
 
-as_url='https://services-atm-test-site2.lsd.ufcg.edu.br/as'
+as_url=''
 as_token_endpoint= as_url + '/tokens'
 as_token_username=
 as_token_password=
