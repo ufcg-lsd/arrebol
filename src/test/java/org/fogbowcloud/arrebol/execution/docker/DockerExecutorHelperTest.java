17adb4eeeedf02d326f008ed027f02d4ca903c94
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-07-25 11:42:42 -0300

    Added setup to DockerExecutorHelperTest and refactored

--- src/test/java/org/fogbowcloud/arrebol/execution/docker/DockerExecutorHelperTest.java
+++ src/test/java/org/fogbowcloud/arrebol/execution/docker/DockerExecutorHelperTest.java
@@ -20,21 +20,24 @@ import org.fogbowcloud.arrebol.execution.docker.request.WorkerDockerRequestHelpe
 import org.fogbowcloud.arrebol.models.task.Task;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.mockito.Mockito;
 import org.springframework.core.io.ClassPathResource;
 
 public class DockerExecutorHelperTest {
 
-    String badFormattedSameLineContent;
-    String emptyContent;
-    String badFormattedContent;
-    String wellFormattedContent;
-    String taskScriptContent;
+    private static String badFormattedSameLineContent;
+    private static String emptyContent;
+    private static String badFormattedContent;
+    private static String wellFormattedContent;
+    private static String taskScriptExecutorContent;
+    private static String expectedWriteTsExecutorCommand;
+
     private Task task;
 
-    @Before
-    public void setUp() throws IOException {
+    @BeforeClass
+    public static void setup() throws IOException {
         final String TASK_SCRIPT_EXAMPLES_DIRECTORY = "task-script-examples/";
         final String EXIT_CODES_SAME_LINE_FILE_PATH =
             TASK_SCRIPT_EXAMPLES_DIRECTORY + "ec-in-same-line.ts.ec";
@@ -44,35 +47,41 @@ public class DockerExecutorHelperTest {
             TASK_SCRIPT_EXAMPLES_DIRECTORY + "gap-in-ecs.ts.ec";
         final String WELL_FORMED_FILE_PATH = TASK_SCRIPT_EXAMPLES_DIRECTORY + "well-formed.ts.ec";
 
-        this.badFormattedSameLineContent = getFileContent(
+        badFormattedSameLineContent = getFileContent(
             new ClassPathResource(EXIT_CODES_SAME_LINE_FILE_PATH));
-        this.emptyContent = getFileContent(new ClassPathResource(EMPTY_EXAMPLE_FILE_PATH));
-        this.badFormattedContent = getFileContent(
+        emptyContent = getFileContent(new ClassPathResource(EMPTY_EXAMPLE_FILE_PATH));
+        badFormattedContent = getFileContent(
             new ClassPathResource(GAP_IN_EXIT_CODES_FILE_PATH));
-        this.wellFormattedContent = getFileContent(new ClassPathResource(WELL_FORMED_FILE_PATH));
-        this.taskScriptContent = loadTaskScriptExecutor();
+        wellFormattedContent = getFileContent(new ClassPathResource(WELL_FORMED_FILE_PATH));
+        taskScriptExecutorContent = loadTaskScriptExecutor();
+        expectedWriteTsExecutorCommand =
+            "echo '" + taskScriptExecutorContent + "' > /tmp/" + TASK_SCRIPT_EXECUTOR_NAME;
+    }
+
+
+    @Before
+    public void init() {
         this.task = mockTask();
     }
 
     @Test
     public void testNotEmptyContent() {
-        Assert.assertFalse(this.wellFormattedContent.isEmpty());
-        Assert.assertFalse(this.badFormattedContent.isEmpty());
-        Assert.assertFalse(this.badFormattedSameLineContent.isEmpty());
+        Assert.assertFalse(wellFormattedContent.isEmpty());
+        Assert.assertFalse(badFormattedContent.isEmpty());
+        Assert.assertFalse(badFormattedSameLineContent.isEmpty());
     }
 
     @Test
     public void testEmptyContent() {
-        Assert.assertTrue(this.emptyContent.isEmpty());
+        Assert.assertTrue(emptyContent.isEmpty());
     }
 
     @Test
     public void testSendTaskScriptExecutor() throws Exception {
         DockerCommandExecutor dockerCommandExecutor = Mockito.mock(DockerCommandExecutor.class);
 
-        String expectedWriteCommand =
-            "echo '" + this.taskScriptContent + "' > /tmp/" + TASK_SCRIPT_EXECUTOR_NAME;
-        Mockito.when(dockerCommandExecutor.executeCommand(Mockito.eq(expectedWriteCommand)))
+        Mockito
+            .when(dockerCommandExecutor.executeCommand(Mockito.eq(expectedWriteTsExecutorCommand)))
             .thenReturn(mockSuccessExecInstanceResult);
 
         DockerExecutorHelper dockerExecutorHelper = mockSpyDockerExecutorHelper(
@@ -84,10 +93,8 @@ public class DockerExecutorHelperTest {
     public void testFailCommandSendTaskScriptExecutor() throws Exception {
         DockerCommandExecutor dockerCommandExecutor = Mockito.mock(DockerCommandExecutor.class);
 
-        String expectedWriteCommand =
-            "echo '" + this.taskScriptContent + "' > /tmp/" + TASK_SCRIPT_EXECUTOR_NAME;
         Mockito.when(dockerCommandExecutor
-            .executeCommand(Mockito.eq(expectedWriteCommand)))
+            .executeCommand(Mockito.eq(expectedWriteTsExecutorCommand)))
             .thenReturn(mockFailExecInstanceResult);
 
         DockerExecutorHelper dockerExecutorHelper = mockSpyDockerExecutorHelper(
@@ -133,7 +140,8 @@ public class DockerExecutorHelperTest {
             .thenReturn(mockSuccessExecInstanceResult);
 
         DockerExecutorHelper dockerExecutorHelper = Mockito
-            .spy(new DockerExecutorHelper(this.taskScriptContent, workerDockerRequestHelper));
+            .spy(new DockerExecutorHelper(taskScriptExecutorContent,
+                workerDockerRequestHelper));
         Assert.assertEquals(mockEcFileContent, dockerExecutorHelper.getEcFile(mockEcFilePath));
     }
 
@@ -154,7 +162,8 @@ public class DockerExecutorHelperTest {
         DockerCommandExecutor dockerCommandExecutor) throws UnsupportedEncodingException {
         WorkerDockerRequestHelper workerDockerRequestHelper = mockWorkerDockerRequestHelper();
         DockerExecutorHelper dockerExecutorHelper = Mockito
-            .spy(new DockerExecutorHelper(this.taskScriptContent, workerDockerRequestHelper));
+            .spy(new DockerExecutorHelper(taskScriptExecutorContent,
+                workerDockerRequestHelper));
         dockerExecutorHelper.setDockerCommandExecutor(dockerCommandExecutor);
         return dockerExecutorHelper;
     }
