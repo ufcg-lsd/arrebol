0183f2965ae6cf187bf98b004b892c493e4b616c
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-08-02 10:18:47 -0300

    Added doc to DockerContainerResource

--- src/main/java/org/fogbowcloud/arrebol/execution/docker/resource/DockerContainerResource.java
+++ src/main/java/org/fogbowcloud/arrebol/execution/docker/resource/DockerContainerResource.java
@@ -1,11 +1,18 @@
 package org.fogbowcloud.arrebol.execution.docker.resource;
 
-
+/**
+ * This interface represents a Docker container resource. It controls container initiation and
+ * stopping. Contains an id to container and an API address from docker. The container start with a
+ * {@link ContainerSpecification}, which specifies properties such as imageId, memory, and more for
+ * the container.
+ */
 public interface DockerContainerResource {
 
     void start(ContainerSpecification containerSpecification) throws Exception;
-    void stop() throws Exception;
-    String getId();
-    String getApiAddress();
 
+    void stop() throws Exception;
+
+    String getId();
+
+    String getApiAddress();
 }
