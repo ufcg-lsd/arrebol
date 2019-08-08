13f2b85c46f07545731a41bbafd4417bbebd58c2
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2019-08-08 10:20:11 -0300

    Created DockerFileHandlerHelper

---
+++ src/main/java/org/fogbowcloud/arrebol/execution/docker/request/DockerFileHandlerHelper.java
@@ -0,0 +1,41 @@
+package org.fogbowcloud.arrebol.execution.docker.request;
+
+import org.fogbowcloud.arrebol.execution.docker.DockerCommandExecutor;
+
+public class DockerFileHandlerHelper {
+
+    private static final String COMMAND_WRITE_TO_FILE_PATTERN = "echo '%s' >> %s";
+    private static final String COMMAND_READ_FILE_PATTERN = "cat %s";
+    private String apiAddress;
+    private DockerCommandExecutor dockerCommandExecutor;
+
+    public DockerFileHandlerHelper(String apiAddress, DockerCommandExecutor dockerCommandExecutor) {
+        this.dockerCommandExecutor = dockerCommandExecutor;
+        this.apiAddress = apiAddress;
+    }
+
+    public Integer writeToFile(String containerId, String content, String filePath) {
+        String command = String.format(COMMAND_WRITE_TO_FILE_PATTERN, content, filePath);
+        try {
+            Integer exitCode =
+                    this.dockerCommandExecutor
+                            .executeCommand(apiAddress, containerId, command)
+                            .getExitCode();
+            return exitCode;
+        } catch (Exception e) {
+            throw new RuntimeException("Error while write to file [" + filePath + "] in container [" + containerId + "]");
+        }
+    }
+
+    public String readFile(String containerId, String filePath) {
+        String command = String.format(COMMAND_READ_FILE_PATTERN, filePath);
+        try {
+            String fileContent =
+                    this.dockerCommandExecutor.executeCommandWithStout(
+                            apiAddress, containerId, command);
+            return fileContent;
+        } catch (Exception e) {
+            throw new RuntimeException("Error while read file [" + filePath + "] from container [" + containerId + "]");
+        }
+    }
+}
--- src/main/java/org/fogbowcloud/arrebol/execution/docker/tasklet/TaskletHelper.java
+++ src/main/java/org/fogbowcloud/arrebol/execution/docker/tasklet/TaskletHelper.java
@@ -4,40 +4,45 @@ import java.util.List;
 import org.apache.log4j.Logger;
 import org.fogbowcloud.arrebol.execution.TaskExecutionResult;
 import org.fogbowcloud.arrebol.execution.docker.DockerCommandExecutor;
+import org.fogbowcloud.arrebol.execution.docker.request.DockerFileHandlerHelper;
 import org.fogbowcloud.arrebol.models.command.Command;
 
 public class TaskletHelper {
     private static final String TASK_SCRIPT_EXECUTOR_FILE_PATH = "/tmp/task-script-executor.sh";
     private static final String TASK_SCRIPT_FILE_PATH_PATTERN = "/tmp/%s.ts";
     private static final String EC_FILE_PATH_PATTERN = "/tmp/%s.ts.ec";
+    private static final String RUN_TS_EXECUTOR_PATTERN = "/bin/bash %s -d -tsf=%s";
     private final Logger LOGGER = Logger.getLogger(TaskletHelper.class);
 
     private String apiAddress;
     private String containerId;
     private DockerCommandExecutor dockerCommandExecutor;
+    private DockerFileHandlerHelper dockerFileHandlerHelper;
 
     public TaskletHelper(String apiAddress, String containerId) {
         this.apiAddress = apiAddress;
         this.containerId = containerId;
         this.dockerCommandExecutor = new DockerCommandExecutor();
+        this.dockerFileHandlerHelper =
+                new DockerFileHandlerHelper(apiAddress, dockerCommandExecutor);
     }
 
     public void runTaskScriptExecutor(String taskId) throws Exception {
-        String taskScriptFilePath = String.format(TASK_SCRIPT_FILE_PATH_PATTERN, taskId);
-        this.dockerCommandExecutor.executeAsyncCommand(
-                apiAddress,
-                containerId,
-                "/bin/bash " + TASK_SCRIPT_EXECUTOR_FILE_PATH + " -d -tsf=" + taskScriptFilePath);
+        String taskScriptExecutorFilePath = String.format(TASK_SCRIPT_FILE_PATH_PATTERN, taskId);
+        String command =
+                String.format(
+                        RUN_TS_EXECUTOR_PATTERN,
+                        TASK_SCRIPT_EXECUTOR_FILE_PATH,
+                        taskScriptExecutorFilePath);
+        this.dockerCommandExecutor.executeAsyncCommand(apiAddress, containerId, command);
     }
 
     public void sendTaskScriptExecutor(String taskScriptExecutor) throws Exception {
         LOGGER.debug("Sending Task Script Executor to Docker Worker");
-        String writeCommand = "echo '" + taskScriptExecutor + "' > " + TASK_SCRIPT_EXECUTOR_FILE_PATH;
         try {
             int exitCode =
-                    this.dockerCommandExecutor
-                            .executeCommand(apiAddress, containerId, writeCommand)
-                            .getExitCode();
+                    dockerFileHandlerHelper.writeToFile(
+                            containerId, taskScriptExecutor, TASK_SCRIPT_EXECUTOR_FILE_PATH);
             if (exitCode != 0) {
                 throw new Exception(
                         "Error while trying to execute send task script executor, exit code ["
@@ -52,47 +57,41 @@ public class TaskletHelper {
 
     public void sendTaskScript(String taskId, List<Command> commands) throws Exception {
         String taskScriptFilePath = String.format(TASK_SCRIPT_FILE_PATH_PATTERN, taskId);
+        String taskScriptContent = commandsToString(commands);
         LOGGER.debug("Starting to write commands to ts file path [" + taskScriptFilePath + "].");
-        int[] deliveryResults = writeCommandsToTaskScript(commands, taskScriptFilePath);
-
-        for (int i = 0; i < commands.size(); i++) {
-            if (deliveryResults[i] != 0) {
+        try {
+            int exitCode =
+                    dockerFileHandlerHelper.writeToFile(
+                            containerId, taskScriptContent, taskScriptFilePath);
+            if (exitCode != 0) {
                 throw new Exception(
-                        "Error while trying to send command ["
-                                + commands.get(i)
-                                + "] exit code="
-                                + deliveryResults[i]);
+                        "Error while trying to send task script ["
+                                + taskId
+                                + "] to container ["
+                                + containerId
+                                + "]; exit code ["
+                                + exitCode
+                                + "]");
             }
+        } catch (Throwable e) {
+            LOGGER.error(e);
+            throw new Exception(
+                    "Cannot send task script [" + taskId + "] to container [" + containerId + "]");
         }
     }
 
-    private int[] writeCommandsToTaskScript(List<Command> commands, String tsFilePath) {
-        int[] exitCodes = new int[commands.size()];
-        int i = 0;
-        for (Command cmd : commands) {
-            Integer exitCode;
-            try {
-                exitCode = writeToFile(cmd.getCommand(), tsFilePath);
-            } catch (Throwable t) {
-                exitCode = TaskExecutionResult.UNDETERMINED_RESULT;
-            }
-            exitCodes[i++] = exitCode;
+    private String commandsToString(List<Command> commands) {
+        String result = "";
+        for (Command c : commands) {
+            result += c.getCommand() + "\n";
         }
-        return exitCodes;
-    }
-
-    private Integer writeToFile(String command, String file) throws Exception {
-        return this.dockerCommandExecutor
-                .executeCommand(apiAddress, containerId, "echo '" + command + "' >> " + file)
-                .getExitCode();
+        result = result.trim();
+        return result;
     }
 
     public int[] getExitCodes(String taskId, Integer size) throws Exception {
         String ecFilePath = String.format(EC_FILE_PATH_PATTERN, taskId);
-        String commandToGetFile = String.format("cat %s", ecFilePath);
-        String ecFileContent =
-                this.dockerCommandExecutor.executeCommandWithStout(
-                        apiAddress, containerId, commandToGetFile);
+        String ecFileContent = this.dockerFileHandlerHelper.readFile(containerId, ecFilePath);
         String[] strExitcodes = ecFileContent.split("\r\n");
         int[] exitcodes = new int[size];
         if (!ecFileContent.trim().isEmpty()) {
