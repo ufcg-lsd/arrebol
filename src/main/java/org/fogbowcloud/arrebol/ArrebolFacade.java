f2b93c198407fde0cfaf7bbd79521e2e47fa71fd
Emanuel Joívo <emanueljoivo@gmail.com>
2019-11-20 15:01:33 -0300

    Sync methods

--- src/main/java/org/fogbowcloud/arrebol/ArrebolFacade.java
+++ src/main/java/org/fogbowcloud/arrebol/ArrebolFacade.java
@@ -1,34 +1,34 @@
 package org.fogbowcloud.arrebol;
 
-import java.util.List;
-import java.util.Map;
 import org.fogbowcloud.arrebol.models.job.Job;
 import org.fogbowcloud.arrebol.models.task.TaskState;
 import org.fogbowcloud.arrebol.processor.dto.DefaultJobProcessorDTO;
 import org.fogbowcloud.arrebol.processor.spec.JobProcessorSpec;
 import org.fogbowcloud.arrebol.processor.spec.WorkerNode;
 
+import java.util.List;
+
 public class ArrebolFacade {
 
     private ArrebolController arrebolController;
 
-    public ArrebolFacade(ArrebolController arrebolController){
+    public ArrebolFacade(ArrebolController arrebolController) {
         this.arrebolController = arrebolController;
     }
 
-    public void start(){
+    public void start() {
         arrebolController.start();
     }
 
-    public  void stop(){
+    public void stop() {
         arrebolController.stop();
     }
 
-    public String createQueue(JobProcessorSpec jobProcessorSpec){
+    public synchronized String createQueue(JobProcessorSpec jobProcessorSpec) {
         return arrebolController.createQueue(jobProcessorSpec);
     }
 
-    public List<DefaultJobProcessorDTO> getQueues(){
+    public List<DefaultJobProcessorDTO> getQueues() {
         return arrebolController.getQueues();
     }
 
@@ -36,24 +36,24 @@ public class ArrebolFacade {
         return arrebolController.getQueue(queueId);
     }
 
-    public void addWorkers(String queueId, WorkerNode workerNode) {
+    public synchronized void addWorkers(String queueId, WorkerNode workerNode) {
         arrebolController.addWorkers(queueId, workerNode);
     }
 
-    public String addJob(String queue, Job job){
+    public synchronized String addJob(String queue, Job job) {
         return arrebolController.addJob(queue, job);
     }
 
-    public Job getJob(String queueId, String jobId){
+    public Job getJob(String queueId, String jobId) {
         return arrebolController.getJob(queueId, jobId);
     }
 
-    public void stopJob(Job job){
+    public void stopJob(Job job) {
         arrebolController.stopJob(job);
     }
 
     //TODO: do we really need this?
-    public TaskState getTaskState(String taskId){
+    public TaskState getTaskState(String taskId) {
         return arrebolController.getTaskState(taskId);
     }
 }
