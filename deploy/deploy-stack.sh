ebbbbe5499d6a69e5c0912f5aecd8edab4783055
Wesley Monte <wesley.monte@ccc.ufcg.edu.br>
2020-05-06 20:04:33 -0300

    Added stop service function to deploy-stack.sh

--- deploy/deploy-stack.sh
+++ deploy/deploy-stack.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-sudo docker stack deploy -c $(pwd)/docker-stack.yml lsd
+start() {
+  echo "--> Creating arrebol deployment..."
+  docker-compose -f $(pwd)/docker-stack.yml up -d
+}
+
+stop() {
+  echo "--> Removing arrebol deployment..."
+  docker-compose -f $(pwd)/docker-stack.yml down
+}
+
+define_param() {
+  case $1 in
+    start)
+      start
+      ;;
+    stop)
+      stop
+      ;;
+  esac
+}
+
+define_param "$@"
