944a53572e8fe3bc92c744b566718c5f03f04d3c
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-07-09 18:11:25 -0300

    Fixed code to get new k8s cluster namespace info

--- src/main/java/org/fogbowcloud/arrebol/execution/k8s/K8sConfiguration.java
+++ src/main/java/org/fogbowcloud/arrebol/execution/k8s/K8sConfiguration.java
@@ -7,27 +7,34 @@ import org.fogbowcloud.arrebol.models.configuration.Property;
 public class K8sConfiguration {
 	private final Integer capacity;
     private final String address;
+    private final String namespace;
     
     public K8sConfiguration(Configuration configuration) throws Exception {
         checkK8sConfigurationProperties(configuration);
         Property<Double> capacity = configuration.getProperty(K8sConstants.JSON_CLUSTER_CAPACITY_KEY);
         Property<String> address = configuration.getProperty(K8sConstants.JSON_CLUSTER_ADDRESS_KEY);
+        Property<String> namespace = configuration.getProperty(K8sConstants.JSON_CLUSTER_NAMESPACE_KEY);
         this.capacity = capacity.getValue().intValue();
         this.address = address.getValue();
+        this.namespace = namespace.getValue();
     }
 
 	private void checkK8sConfigurationProperties(Configuration configuration) throws Exception {
 		String verifyMsg = " Please, verify your configuration file.";
 		String capacityMsg = "K8s cluster capacity configuration property wrong or missing." + verifyMsg;
 		String addressMsg = "K8s cluster address configuration property wrong or missing." + verifyMsg;
+		String namespaceMsg = "K8s cluster namespace configuration property wrong or missing." + verifyMsg;
 
         Property<Double> capacity = configuration.getProperty(K8sConstants.JSON_CLUSTER_CAPACITY_KEY);
         Property<String> address = configuration.getProperty(K8sConstants.JSON_CLUSTER_ADDRESS_KEY);
+        Property<String> namespace = configuration.getProperty(K8sConstants.JSON_CLUSTER_NAMESPACE_KEY);
         
         if (address.getValue() == null || address.getValue().trim().isEmpty()) {
             throw new Exception(addressMsg);
         } else if (capacity.getValue() == null || capacity.getValue().intValue() == 0) {
             throw new Exception(capacityMsg);
+        } else if (namespace.getValue() == null || namespace.getValue().trim().isEmpty()) {
+            throw new Exception(namespaceMsg);
         }
 	}
 
@@ -37,6 +44,10 @@ public class K8sConfiguration {
 
 	public String getAddress() {
 		return address;
+	}
+
+	public String getNamespace() {
+		return namespace;
 	}
 
 }
