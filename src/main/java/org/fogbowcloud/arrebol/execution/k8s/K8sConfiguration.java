db048f3db89201ff25105dd5f2901520f6c7d444
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-07-09 17:36:12 -0300

    Confusion fixed when obtaining property

--- src/main/java/org/fogbowcloud/arrebol/execution/k8s/K8sConfiguration.java
+++ src/main/java/org/fogbowcloud/arrebol/execution/k8s/K8sConfiguration.java
@@ -10,7 +10,7 @@ public class K8sConfiguration {
     
     public K8sConfiguration(Configuration configuration) throws Exception {
         checkK8sConfigurationProperties(configuration);
-        Property<Double> capacity = configuration.getProperty(K8sConstants.JSON_WORKERS_CAPACITY_KEY);
+        Property<Double> capacity = configuration.getProperty(K8sConstants.JSON_CLUSTER_CAPACITY_KEY);
         Property<String> address = configuration.getProperty(K8sConstants.JSON_CLUSTER_ADDRESS_KEY);
         this.capacity = capacity.getValue().intValue();
         this.address = address.getValue();
@@ -21,13 +21,12 @@ public class K8sConfiguration {
 		String capacityMsg = "K8s cluster capacity configuration property wrong or missing." + verifyMsg;
 		String addressMsg = "K8s cluster address configuration property wrong or missing." + verifyMsg;
 
-        Property<Double> capacity = configuration.getProperty(K8sConstants.JSON_CLUSTER_ADDRESS_KEY);
-        Property<String> address = configuration.getProperty(K8sConstants.JSON_WORKERS_CAPACITY_KEY);
+        Property<Double> capacity = configuration.getProperty(K8sConstants.JSON_CLUSTER_CAPACITY_KEY);
+        Property<String> address = configuration.getProperty(K8sConstants.JSON_CLUSTER_ADDRESS_KEY);
         
-
-        if (address.getValue() == null || address.getValue().trim().isEmpty() || address.getValue().contains(":")) {
+        if (address.getValue() == null || address.getValue().trim().isEmpty()) {
             throw new Exception(addressMsg);
-        } else if (capacity.getValue() == null || capacity.getValue().intValue() <= 0) {
+        } else if (capacity.getValue() == null || capacity.getValue().intValue() == 0) {
             throw new Exception(capacityMsg);
         }
 	}
