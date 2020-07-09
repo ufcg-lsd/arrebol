2aa9b2374f1fb27d7dde37445d276aebf35010f8
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-07-09 20:31:53 -0300

    List jobs method removed from K8s client interface

--- src/main/java/org/fogbowcloud/arrebol/execution/k8s/client/K8sClient.java
+++ src/main/java/org/fogbowcloud/arrebol/execution/k8s/client/K8sClient.java
@@ -1,16 +1,12 @@
 package org.fogbowcloud.arrebol.execution.k8s.client;
 
 import io.kubernetes.client.openapi.models.V1Job;
-import io.kubernetes.client.openapi.models.V1JobList;
 import io.kubernetes.client.openapi.models.V1Status;
-import java.util.List;
 
 public interface K8sClient {
     
 	//FIXME what does it return?
 	//??? createNamespace();
-	
-    V1JobList listJobs();
     
     V1Job createJob(String name, String containerImage, List<String> command);
     
