2c8cdcfd1e76497e1564c39f4c57df25b119deb9
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-07-09 18:09:17 -0300

    New constant added to inform k8s cluster namespace to be used

--- src/main/java/org/fogbowcloud/arrebol/execution/k8s/constants/K8sConstants.java
+++ src/main/java/org/fogbowcloud/arrebol/execution/k8s/constants/K8sConstants.java
@@ -3,5 +3,6 @@ package org.fogbowcloud.arrebol.execution.k8s.constants;
 public class K8sConstants {
 	public static final String JSON_CLUSTER_CAPACITY_KEY = "capacity";
 	public static final String JSON_CLUSTER_ADDRESS_KEY = "address";
+	public static final String JSON_CLUSTER_NAMESPACE_KEY = "namespace";
 
 }
