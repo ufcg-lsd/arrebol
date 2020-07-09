0a49e47603dc35c5e4526c6fea056c4e1362ebac
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-07-09 18:42:31 -0300

    Default K8s cluster resource class added

---
+++ src/main/java/org/fogbowcloud/arrebol/execution/k8s/resource/DefaultK8sClusterResource.java
@@ -0,0 +1,28 @@
+package org.fogbowcloud.arrebol.execution.k8s.resource;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class DefaultK8sClusterResource implements K8sClusterResource {
+
+	@Id
+	private String id;
+	private String address;
+	
+	public DefaultK8sClusterResource(String id, String address) {
+		this.id = id;
+		this.address = address;
+	}
+
+	@Override
+	public String getId() {
+		return id;
+	}
+
+	@Override
+	public String getApiAddress() {
+		return address;
+	}
+	
+}
