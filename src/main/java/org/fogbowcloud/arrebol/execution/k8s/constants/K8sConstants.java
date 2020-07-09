a63d3bf1b6bc282328858153a3f0cac8d9aa5e49
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-07-08 23:17:29 -0300

    K8s constant class added

---
+++ src/main/java/org/fogbowcloud/arrebol/execution/k8s/constants/K8sConstants.java
@@ -0,0 +1,7 @@
+package org.fogbowcloud.arrebol.execution.k8s.constants;
+
+public class K8sConstants {
+	public static final String JSON_WORKERS_CAPACITY_KEY = "workerCapacity";
+	public static final String JSON_CLUSTER_ADDRESS_KEY = "clusterAddress";
+
+}
