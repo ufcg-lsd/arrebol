d93d673a19defc7937fda509eb45bce614806eb9
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-07-09 17:33:03 -0300

    Fixed constants value

--- src/main/java/org/fogbowcloud/arrebol/execution/k8s/constants/K8sConstants.java
+++ src/main/java/org/fogbowcloud/arrebol/execution/k8s/constants/K8sConstants.java
@@ -1,7 +1,7 @@
 package org.fogbowcloud.arrebol.execution.k8s.constants;
 
 public class K8sConstants {
-	public static final String JSON_WORKERS_CAPACITY_KEY = "workerCapacity";
-	public static final String JSON_CLUSTER_ADDRESS_KEY = "clusterAddress";
+	public static final String JSON_CLUSTER_CAPACITY_KEY = "capacity";
+	public static final String JSON_CLUSTER_ADDRESS_KEY = "address";
 
 }
