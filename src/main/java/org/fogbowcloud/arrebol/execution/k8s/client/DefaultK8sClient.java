82f02a14e04b5ef554d46fc05d2e953147655ab3
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-07-09 23:12:40 -0300

    Fixed class for changes made to the K8s client interface

--- src/main/java/org/fogbowcloud/arrebol/execution/k8s/client/DefaultK8sClient.java
+++ src/main/java/org/fogbowcloud/arrebol/execution/k8s/client/DefaultK8sClient.java
@@ -3,6 +3,7 @@ package org.fogbowcloud.arrebol.execution.k8s.client;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -11,14 +12,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Transient;
 
+import org.apache.log4j.Logger;
+
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.Configuration;
 import io.kubernetes.client.openapi.apis.BatchV1Api;
 import io.kubernetes.client.openapi.models.V1Container;
-import io.kubernetes.client.openapi.models.V1DeleteOptions;
 import io.kubernetes.client.openapi.models.V1Job;
-import io.kubernetes.client.openapi.models.V1JobList;
 import io.kubernetes.client.openapi.models.V1JobSpec;
 import io.kubernetes.client.openapi.models.V1ObjectMeta;
 import io.kubernetes.client.openapi.models.V1PodSpec;
@@ -26,16 +27,13 @@ import io.kubernetes.client.openapi.models.V1PodTemplateSpec;
 import io.kubernetes.client.openapi.models.V1Status;
 import io.kubernetes.client.util.ClientBuilder;
 
-@Entity
 public class DefaultK8sClient implements K8sClient {
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
 	
 	private String namespace;
 	@Transient
     private BatchV1Api batchApi;
+	
+	private final Logger LOGGER = Logger.getLogger(DefaultK8sClient.class);
 
     public DefaultK8sClient(String host, String namespace) throws IOException {
         ApiClient client = ClientBuilder.defaultClient();
@@ -44,43 +42,24 @@ public class DefaultK8sClient implements K8sClient {
         
         Configuration.setDefaultApiClient(client);
         this.batchApi = new BatchV1Api();
+        this.namespace = namespace;
     }
     
-    @Override
-    //TODO organize this method
-    public V1JobList listJobs() {
-        try {
-            return batchApi.listNamespacedJob(namespace, null, null, null, null, null, null, null, null, null);
-        } catch (ApiException e) {
-            System.out.println("Exception when calling CoreV1Api->listNamespacedJob: " + e.toString());
-            System.out.println("------ Response body: \n" + e.getResponseBody());
-            //System.out.println("------ Stack trace: \n");
-            //e.printStackTrace();
-        }
-        return null;
-    }
+    public DefaultK8sClient() {}
     
     @Override
-    //TODO organize this method
-    public V1Job createJob(String name, String containerImage, List<String> command) {       
-        
+    public V1Job createJob(String name, String imageId, String command) throws ApiException {
+    	List<String> commandList = buildCommands(command);
+    	
         V1PodSpec podSpec = new V1PodSpec()
                 .restartPolicy("Never")
                 .containers(Arrays.asList(
                     new V1Container()
                         .name(name)
-                        .image(containerImage)
-                        .command(command)
-                        /*.volumeMounts(Arrays.asList(
-                                new V1VolumeMount().mountPath("/nfs").name("nfs-job-test").subPath("/nfs")
-                        )*/
+                        .image(imageId)
+                        .command(commandList)
                         )
-                /*).volumes(Arrays.asList(
-                        new V1Volume()
-                                .name(volname)
-                                .persistentVolumeClaim(new V1PersistentVolumeClaimVolumeSource().claimName(volclaim))
-                )*/
-                );
+                	);
         
         V1Job job =  new V1Job()
                 .apiVersion("batch/v1")
@@ -94,38 +73,26 @@ public class DefaultK8sClient implements K8sClient {
                         )
                 );
         
-        try {
-            return batchApi.createNamespacedJob(namespace, job, null, null, null);
-        } catch (ApiException e) {
-            System.out.println("Exception when calling CoreV1Api->createNamespacedJob: " + e.toString());
-            System.out.println("------ Response body: \n" + e.getResponseBody());
-            //System.out.println("------ Stack trace: \n");
-            //e.printStackTrace();
-        }
-        return null;
+        return batchApi.createNamespacedJob(namespace, job, null, null, null);
+        
     }
     
-    @Override
-    //TODO organize this method
-    public V1Status deleteJob(String name) {
-        String pretty = "pretty_example";  // str | If 'true', then the output is pretty printed. (optional);
-        V1DeleteOptions body = new V1DeleteOptions(); // V1DeleteOptions |  (optional);
-        try {
-            return batchApi.deleteNamespacedJob(name, namespace, pretty, null,  null, null, null, body);    
-        } catch (ApiException e) {
-            System.out.println("Exception when calling CoreV1Api->deleteNamespacedJob: " + e.toString());
-            System.out.println("------ Response body: \n" + e.getResponseBody());
-            //System.out.println("------ Stack trace: \n");
-            //e.printStackTrace();
-        }
-        return null;
+    private List<String> buildCommands(String command) {
+		List<String> commands = new LinkedList<String>();
+		commands.add(new String("/bin/sh"));
+		commands.add(new String("-c"));
+		commands.add(new String(command));
+		return commands;
+	}
+
+	@Override
+    public V1Status deleteJob(String name) throws ApiException {
+        return batchApi.deleteNamespacedJob(name, namespace, null, null,  null, null, null, null);    
     }
 
 	@Override
-	//TODO implement this method
-	public V1Job getJob(String name) {
-		// TODO Auto-generated method stub
-		return null;
+	public V1Job getJob(String name) throws ApiException {
+		return batchApi.readNamespacedJob(name, this.namespace, null, null, null);
 	}
 
 }
