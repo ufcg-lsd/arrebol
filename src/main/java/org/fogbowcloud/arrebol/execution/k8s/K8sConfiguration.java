e4e61882636f115c8f6557208f9409e265b3daaa
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-07-10 18:04:30 -0300

    Changed code to check new constant of volume name (opcional)

--- src/main/java/org/fogbowcloud/arrebol/execution/k8s/K8sConfiguration.java
+++ src/main/java/org/fogbowcloud/arrebol/execution/k8s/K8sConfiguration.java
@@ -6,18 +6,21 @@ import org.fogbowcloud.arrebol.models.configuration.Property;
 
 public class K8sConfiguration {
 	private final Integer capacity;
-    private final String address;
-    private final String namespace;
-    
-    public K8sConfiguration(Configuration configuration) throws Exception {
-        checkK8sConfigurationProperties(configuration);
-        Property<Double> capacity = configuration.getProperty(K8sConstants.JSON_CLUSTER_CAPACITY_KEY);
-        Property<String> address = configuration.getProperty(K8sConstants.JSON_CLUSTER_ADDRESS_KEY);
-        Property<String> namespace = configuration.getProperty(K8sConstants.JSON_CLUSTER_NAMESPACE_KEY);
-        this.capacity = capacity.getValue().intValue();
-        this.address = address.getValue();
-        this.namespace = namespace.getValue();
-    }
+	private final String address;
+	private final String namespace;
+	private final String volumeName;
+
+	public K8sConfiguration(Configuration configuration) throws Exception {
+		checkK8sConfigurationProperties(configuration);
+		Property<Double> capacity = configuration.getProperty(K8sConstants.JSON_CLUSTER_CAPACITY_KEY);
+		Property<String> address = configuration.getProperty(K8sConstants.JSON_CLUSTER_ADDRESS_KEY);
+		Property<String> namespace = configuration.getProperty(K8sConstants.JSON_CLUSTER_NAMESPACE_KEY);
+		Property<String> volumeName = configuration.getProperty(K8sConstants.JSON_CLUSTER_VOLUME_NAME_KEY);
+		this.capacity = capacity.getValue().intValue();
+		this.address = address.getValue();
+		this.namespace = namespace.getValue();
+		this.volumeName = volumeName.getValue();
+	}
 
 	private void checkK8sConfigurationProperties(Configuration configuration) throws Exception {
 		String verifyMsg = " Please, verify your configuration file.";
@@ -25,17 +28,17 @@ public class K8sConfiguration {
 		String addressMsg = "K8s cluster address configuration property wrong or missing." + verifyMsg;
 		String namespaceMsg = "K8s cluster namespace configuration property wrong or missing." + verifyMsg;
 
-        Property<Double> capacity = configuration.getProperty(K8sConstants.JSON_CLUSTER_CAPACITY_KEY);
-        Property<String> address = configuration.getProperty(K8sConstants.JSON_CLUSTER_ADDRESS_KEY);
-        Property<String> namespace = configuration.getProperty(K8sConstants.JSON_CLUSTER_NAMESPACE_KEY);
-        
-        if (address.getValue() == null || address.getValue().trim().isEmpty()) {
-            throw new Exception(addressMsg);
-        } else if (capacity.getValue() == null || capacity.getValue().intValue() == 0) {
-            throw new Exception(capacityMsg);
-        } else if (namespace.getValue() == null || namespace.getValue().trim().isEmpty()) {
-            throw new Exception(namespaceMsg);
-        }
+		Property<Double> capacity = configuration.getProperty(K8sConstants.JSON_CLUSTER_CAPACITY_KEY);
+		Property<String> address = configuration.getProperty(K8sConstants.JSON_CLUSTER_ADDRESS_KEY);
+		Property<String> namespace = configuration.getProperty(K8sConstants.JSON_CLUSTER_NAMESPACE_KEY);
+
+		if (address.getValue() == null || address.getValue().trim().isEmpty()) {
+			throw new Exception(addressMsg);
+		} else if (capacity.getValue() == null || capacity.getValue().intValue() == 0) {
+			throw new Exception(capacityMsg);
+		} else if (namespace.getValue() == null || namespace.getValue().trim().isEmpty()) {
+			throw new Exception(namespaceMsg);
+		}
 	}
 
 	public Integer getCapacity() {
@@ -48,6 +51,10 @@ public class K8sConfiguration {
 
 	public String getNamespace() {
 		return namespace;
+	}
+
+	public String getVolumeName() {
+		return volumeName;
 	}
 
 }
