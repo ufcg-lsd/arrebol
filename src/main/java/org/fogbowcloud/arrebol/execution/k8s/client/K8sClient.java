1e49733e3c034ba641351c22911461b917a750d0
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-08-07 13:20:43 -0300

    Removed unused method

--- src/main/java/org/fogbowcloud/arrebol/execution/k8s/client/K8sClient.java
+++ src/main/java/org/fogbowcloud/arrebol/execution/k8s/client/K8sClient.java
@@ -5,10 +5,7 @@ import io.kubernetes.client.openapi.models.V1Job;
 import io.kubernetes.client.openapi.models.V1Status;
 
 public interface K8sClient {
-    
-	//FIXME what does it return?
-	//??? createNamespace();
-    
+
     V1Job createJob(String name, String containerImage, String command) throws ApiException;
     
     V1Status deleteJob(String name) throws ApiException;
