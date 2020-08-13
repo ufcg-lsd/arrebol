354abba5b39be14a8614e90c2a58b8a60cf0e2f9
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-08-13 16:26:41 -0300

    Changed return type to K8sJob

--- src/main/java/org/fogbowcloud/arrebol/execution/k8s/client/K8sClient.java
+++ src/main/java/org/fogbowcloud/arrebol/execution/k8s/client/K8sClient.java
@@ -1,15 +1,17 @@
 package org.fogbowcloud.arrebol.execution.k8s.client;
 
+import org.fogbowcloud.arrebol.execution.k8s.models.K8sJob;
+
 import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.models.V1Job;
 import io.kubernetes.client.openapi.models.V1Status;
 
 public interface K8sClient {
 
-    V1Job createJob(String name, String imageId, String memoryRequest, String cpuRequest, String command) throws ApiException;
+	K8sJob createJob(String name, String imageId, String memoryRequest, String cpuRequest, String command) throws ApiException;
     
     V1Status deleteJob(String name) throws ApiException;
     
-    V1Job getJob(String name) throws ApiException;
+    K8sJob getJob(String name) throws ApiException;
     
 }
