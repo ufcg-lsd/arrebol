e497158227e65b569ac4dba9d1421891ddd5955f
thiagowhispher <thiago.souza@ccc.ufcg.edu.br>
2020-08-13 16:28:16 -0300

    Changed code to use new K8sJob class

--- src/main/java/org/fogbowcloud/arrebol/execution/k8s/client/DefaultK8sClient.java
+++ src/main/java/org/fogbowcloud/arrebol/execution/k8s/client/DefaultK8sClient.java
@@ -1,6 +1,7 @@
 package org.fogbowcloud.arrebol.execution.k8s.client;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
@@ -15,21 +16,29 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Transient;
 
+import org.apache.http.Header;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.log4j.Logger;
+import org.fogbowcloud.arrebol.execution.k8s.http.HttpWrapper;
+import org.fogbowcloud.arrebol.execution.k8s.models.K8sContainer;
+import org.fogbowcloud.arrebol.execution.k8s.models.K8sJob;
+import org.fogbowcloud.arrebol.execution.k8s.models.K8sJobSpec;
+import org.fogbowcloud.arrebol.execution.k8s.models.K8sObjectMeta;
+import org.fogbowcloud.arrebol.execution.k8s.models.K8sPodSpec;
+import org.fogbowcloud.arrebol.execution.k8s.models.K8sPodTemplateSpec;
+import org.fogbowcloud.arrebol.execution.k8s.models.K8sResourceRequirements;
 
-import io.kubernetes.client.custom.Quantity;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.ApiException;
 import io.kubernetes.client.openapi.Configuration;
 import io.kubernetes.client.openapi.apis.BatchV1Api;
-import io.kubernetes.client.openapi.models.V1Container;
-import io.kubernetes.client.openapi.models.V1Job;
-import io.kubernetes.client.openapi.models.V1JobSpec;
 import io.kubernetes.client.openapi.models.V1ObjectMeta;
 import io.kubernetes.client.openapi.models.V1PersistentVolumeClaimVolumeSource;
-import io.kubernetes.client.openapi.models.V1PodSpec;
-import io.kubernetes.client.openapi.models.V1PodTemplateSpec;
-import io.kubernetes.client.openapi.models.V1ResourceRequirements;
 import io.kubernetes.client.openapi.models.V1Status;
 import io.kubernetes.client.openapi.models.V1Volume;
 import io.kubernetes.client.openapi.models.V1VolumeMount;
@@ -50,6 +59,9 @@ public class DefaultK8sClient implements K8sClient {
 	private String volumeName; 
 	
 	@Transient
+	private final Gson gson;
+	
+	@Transient
 	private final Logger LOGGER = Logger.getLogger(DefaultK8sClient.class);
 
     public DefaultK8sClient(String host, String namespace, String volumeName) throws IOException {
@@ -61,20 +73,23 @@ public class DefaultK8sClient implements K8sClient {
         this.batchApi = new BatchV1Api();
         this.namespace = namespace;
         this.volumeName = volumeName;
+        this.gson = new GsonBuilder().create();
     }
     
-    public DefaultK8sClient() {}
+    public DefaultK8sClient() {
+    	this.gson = new GsonBuilder().create();
+    }
     
     @Override
-    public V1Job createJob(String name, String imageId, String memoryRequest, String cpuRequest, String command) throws ApiException {
+    public K8sJob createJob(String name, String imageId, String memoryRequest, String cpuRequest, String command) throws ApiException {
     	List<String> commandList = buildCommands(command);
-    	V1Container podContainer = new V1Container()
+    	K8sContainer podContainer = new K8sContainer()
     			.name(name)
                 .image(imageId)
                 .command(commandList);
     		
     	
-        V1PodSpec podSpec = new V1PodSpec()
+        K8sPodSpec podSpec = new K8sPodSpec()
                 .restartPolicy("Never")
                 .containers(Arrays.asList(
                 		podContainer
@@ -101,32 +116,69 @@ public class DefaultK8sClient implements K8sClient {
     	}
         
         if(Objects.nonNull(cpuRequest) && Objects.nonNull(memoryRequest)) {
-        	V1ResourceRequirements requests = new V1ResourceRequirements();
+        	final K8sResourceRequirements resources = new K8sResourceRequirements();
+        	final Map<String, String> requests = new HashMap<>();
         	
-        	Quantity cpu = new Quantity(cpuRequest);
-        	requests.putRequestsItem("cpu", cpu);
+        	requests.put("cpu", cpuRequest);
+        	requests.put("memory", memoryRequest);
         	
-        	Quantity memory = new Quantity(memoryRequest);
-        	requests.putRequestsItem("memory", memory);
+        	final Map<String, String> limits = new HashMap<>();
         	
-        	podContainer.resources(requests);
+        	limits.put("cpu", cpuRequest);
+        	limits.put("memory", memoryRequest);
+        	
+        	resources.requests(requests);
+        	resources.limits(limits);
+        	podContainer.resources(resources);
         }
         
-        V1Job job =  new V1Job()
+        K8sJob job =  new K8sJob()
                 .apiVersion("batch/v1")
                 .kind("Job")
                 .metadata(
-                        new V1ObjectMeta().name(name))
-                .spec(new V1JobSpec()
-                        .template(new V1PodTemplateSpec()
+                        new K8sObjectMeta().name(name))
+                .spec(new K8sJobSpec()
+                        .template(new K8sPodTemplateSpec()
                             .metadata(new V1ObjectMeta().labels(Collections.singletonMap("app",name)))
                             .spec(podSpec)
                         ).backoffLimit(2)
                 );
         
-        return batchApi.createNamespacedJob(namespace, job, null, null, null);
-        
+        StringEntity requestBody;
+
+		try {
+			requestBody = makeJSONBody(job);
+		} catch (UnsupportedEncodingException e) {
+			throw new ApiException("Job is not well formed to built JSON.");
+		}
+		
+		final String jobEndpoint = this.batchApi.getApiClient().getBasePath() + "/apis/batch/v1/namespaces/default/jobs";
+		K8sJob resultJob = null;
+		
+		try {
+			final String jsonResponse = HttpWrapper.doRequest(HttpPost.METHOD_NAME, jobEndpoint,
+					new LinkedList<Header>(), requestBody);
+			
+			LOGGER.debug("Result submit job [" + name + "] :" + jsonResponse);
+			resultJob = this.gson.fromJson(jsonResponse, K8sJob.class);
+
+			LOGGER.info("Job was submitted with success to K8s cluster: " + resultJob);
+		} catch (Exception e) {
+			throw new ApiException("Submit Job to K8s cluster has FAILED: " + e.getMessage());
+		}
+		
+		return resultJob;
     }
+    
+    private StringEntity makeJSONBody(K8sJob job) throws UnsupportedEncodingException {
+		Gson gson = new GsonBuilder().setPrettyPrinting().create();
+		String json = gson.toJson(job);
+
+		LOGGER.info("JSON body: " + json);
+
+		return new StringEntity(json);
+	}
+
     
     private List<String> buildCommands(String command) {
 		List<String> commands = new LinkedList<String>();
@@ -142,8 +194,23 @@ public class DefaultK8sClient implements K8sClient {
     }
 
 	@Override
-	public V1Job getJob(String name) throws ApiException {
-		return batchApi.readNamespacedJob(name, this.namespace, null, null, null);
+	public K8sJob getJob(String name) throws ApiException {
+		
+		final String jobEndpoint = this.batchApi.getApiClient().getBasePath() + "/apis/batch/v1/namespaces/default/jobs/" + name;
+		K8sJob resultJob = null;
+		
+		try {
+			final String jsonResponse = HttpWrapper.doRequest(HttpGet.METHOD_NAME, jobEndpoint, null);
+			
+			LOGGER.debug("Result get job [" + name + "] :" + jsonResponse);
+			resultJob = this.gson.fromJson(jsonResponse, K8sJob.class);
+
+			LOGGER.info("Job was getted with success to K8s cluster: " + resultJob);
+
+		} catch (Exception e) {
+			throw new ApiException("Get Job to K8s cluster has FAILED: " + e.getMessage());
+		}
+		return resultJob;
 	}
 
 	public Integer getId() {
