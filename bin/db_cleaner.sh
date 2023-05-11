0a4e5890392b0ec23c70405e0f4c8039494f2c6e
cilasmarques <cilas.marques@ccc.ufcg.edu.br>
2023-05-11 15:34:09 +0200

    updating db_cleaner.sh

--- bin/db_cleaner.sh
+++ bin/db_cleaner.sh
@@ -6,12 +6,15 @@ BIN_PATH=`pwd`/bin
 
 echo "Parando o arrebol ===========" > $BIN_PATH/db_cleaner.log 
 kill `cat $BIN_PATH/shutdown.pid` >> $BIN_PATH/db_cleaner.log
+sleep 1
 
 echo "Limpando o banco ============" >> $BIN_PATH/db_cleaner.log 
-PGPASSWORD=@rrebol psql -h localhost -p 5432 arrebol postgres -a -f $BIN_PATH/db_cleaner.sql >> $BIN_PATH/db_cleaner.log
+PGPASSWORD=@rrebol psql -h localhost -p 5432 arrebol arrebol_db_user -a -f $BIN_PATH/db_cleaner.sql >> $BIN_PATH/db_cleaner.log
+sleep 1
 
 echo "Executando o arrebol ========" >> $BIN_PATH/db_cleaner.log 
 sh $BIN_PATH/start-service.sh >> $BIN_PATH/db_cleaner.log
+sleep 1
 
 echo "Finalizando script ==========" >> $BIN_PATH/db_cleaner.log 
 exit 0
